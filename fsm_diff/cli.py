10f25e2d390ea3e7c89a2ad3497d2e620dace305
Ben Thomasson <bthomass@redhat.com>
2017-10-23 14:33:20 -0400

    Extract function

--- fsm_diff/cli.py
+++ fsm_diff/cli.py
@@ -15,6 +15,44 @@ import yaml
 logger = logging.getLogger('cli')
 
 
+def fsm_diff(a, b, silent=True):
+
+    a_states = {x['label'] for x in a['states']}
+    b_states = {x['label'] for x in b['states']}
+
+    missing_in_a = b_states - a_states
+    missing_in_b = a_states - b_states
+
+
+    if (missing_in_b) and not silent:
+        print "Extra states in a:\n   ", "\n    ".join(list(missing_in_b))
+
+    if (missing_in_a) and not silent:
+        print "Extra states in b:\n   ", "\n    ".join(list(missing_in_a))
+
+    new_states = missing_in_b.union(missing_in_a)
+
+    a_transitions = {tuple(sorted(x.items())) for x in a['transitions']}
+    b_transitions = {tuple(sorted(x.items())) for x in b['transitions']}
+
+    missing_in_a = b_transitions - a_transitions
+    missing_in_b = a_transitions - b_transitions
+
+
+    if (missing_in_b) and not silent:
+        print "Extra transitions in a:\n   ", "\n    ".join(map(str, missing_in_b))
+
+    if (missing_in_a) and not silent:
+        print "Extra transitions in b:\n   ", "\n    ".join(map(str, missing_in_a))
+
+    new_transitions = missing_in_b.union(missing_in_a)
+
+    data = dict(states=[dict(label=x) for x in list(new_states)],
+                transitions=[dict(x) for x in list(new_transitions)])
+
+    return data
+
+
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
@@ -32,38 +70,7 @@ def main(args=None):
     with open(parsed_args['<b>']) as f:
         b = yaml.load(f.read())
 
-    a_states = {x['label'] for x in a['states']}
-    b_states = {x['label'] for x in b['states']}
-
-    missing_in_a = b_states - a_states
-    missing_in_b = a_states - b_states
-
-
-    if (missing_in_b):
-        print "Extra states in a:\n   ", "\n    ".join(list(missing_in_b))
-
-    if (missing_in_a):
-        print "Extra states in b:\n   ", "\n    ".join(list(missing_in_a))
-
-    new_states = missing_in_b.union(missing_in_a)
-
-    a_transitions = {tuple(sorted(x.items())) for x in a['transitions']}
-    b_transitions = {tuple(sorted(x.items())) for x in b['transitions']}
-
-    missing_in_a = b_transitions - a_transitions
-    missing_in_b = a_transitions - b_transitions
-
-
-    if (missing_in_b):
-        print "Extra transitions in a:\n   ", "\n    ".join(map(str, missing_in_b))
-
-    if (missing_in_a):
-        print "Extra transitions in b:\n   ", "\n    ".join(map(str, missing_in_a))
-
-    new_transitions = missing_in_b.union(missing_in_a)
-
-    data = dict(states=[dict(label=x) for x in list(new_states)],
-                transitions=[dict(x) for x in list(new_transitions)])
+    data = fsm_diff(a, b, silent=False)
 
     if parsed_args['<output>']:
         with open(parsed_args['<output>'], 'w') as f:
